28b57ffaa619f2ff9432e4ee1f7a4ac0c513aed6
ConanCodes <alexanderthomas121996@gmail.com>
2022-08-31 19:59:46 -0400

    More new stuff

--- src/Components/Alert.jsx
+++ src/Components/Alert.jsx
@@ -1,19 +1,29 @@
-import { Link } from 'react-router-dom';
+import Navbar from "../Components/Navbar";
 
 function App() {
-    return (
-        <>
-        <div>
-            <h2>Hello, why can't I remove the line above this from certain pages?</h2>
-            <h3> is there such thing as an h3?</h3>
-            <h4>I wonder if there is an h4 too?</h4>
-            <h5>ahh okay so the text just keeps on getting smaller</h5>
-            <h6>Let's see if I can link this back to home and guacc</h6>
-        </div>
-        <Link to="/">Home</Link>
-        <Link to="/guacc">Guacc</Link>
-        </>
-    );
+
+  function sayHello() {
+    alert('Pretty sure I said don\'t click me');
+  }
+
+  function sayHi() {
+    alert('I\'m trying to figure out how to incorporate a calculator in this app and I am struggling');
+  }
+  
+  return (
+    <><Navbar />
+    <div>
+          <h2></h2>
+          <button onClick={sayHello}>
+              Don't Click me!
+          </button>
+          <button onClick={sayHi}>
+               Click me!
+          </button>
+      </div>
+      </>
+  );
 }
+
 
 export default App;
