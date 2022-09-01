19f315ad13fe27f41e4fdec1e2d999514080c6c9
ConanCodes <alexanderthomas121996@gmail.com>
2022-09-01 19:11:12 -0400

    Some new stuff

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+    return <nav className="nav">
+        <a href="/" className="/">Home</a>
+        <ul>
+            <li>
+                <a href="/alert">Alert</a>
+            </li>
+        </ul>
+
+
+
+    </nav>
+}
