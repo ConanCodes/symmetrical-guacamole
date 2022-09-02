e9ba421cd649e36fbf252e78126f009081786fb4
ConanCodes <cohenjl13@gmail.com>
2022-09-01 21:18:43 -0400

    Code Review

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -3,6 +3,18 @@ import { Link } from 'react-router-dom';
 import logo from '../Assets/logo.svg';
 import './App.css';
 import Navbar from "../Components/Navbar";
+
+/** Advice/Todo
+ * Rule of thumb everything should be placed withing your App div
+ * 
+ * <div className="app">
+ * <NavBar/>
+ * ...etc
+ * </div>
+ * 
+ * Doing this will allow you to controll the whole app when we start learning state management
+ * Putting it in the div will allow you to remove the empty tags <> which will be less for the the dom to render, so better performance in the long run
+ */
 export default class App extends PureComponent {
 
   render() {
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,3 +1,10 @@
+/** Advice/Todos
+ * className ties a html block to css for styling, if you dont have styling you dont need it. (App.css)
+ * The <ul> and <li> tags are for lists, uselly bulleted, they do nothing here.
+ * A return block with multiple hmtl/jsx divs should really be in a (...).
+ * I dont want to see functions look like this any more man, what a mess!!
+ */
+
 export default function Navbar() {
     return <nav className="nav">
         <a href="/" className="/">Home</a>
